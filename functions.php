cb4cb26ef3f9c94d8e7e545fde08564953b1ce8b
Andrew Gunn <andrewmgunn26@gmail.com>
2017-05-04 14:25:15 -0500

    r

--- functions.php
+++ functions.php
@@ -1,21 +1,66 @@
 <?php
 // Exit if accessed directly
-if ( !defined( 'ABSPATH' ) ) exit;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
 // BEGIN ENQUEUE PARENT ACTION
 // AUTO GENERATED - Do not modify or remove comment markers above or below:
 
-if ( !function_exists( 'chld_thm_cfg_parent_css' ) ):
-    function chld_thm_cfg_parent_css() {
-        wp_enqueue_style( 'chld_thm_cfg_parent', trailingslashit( get_template_directory_uri() ) . 'style.css', array( 'bootstrap','bootstrap-theme','slicknav','foundation-icons' ) );
-    }
+if ( ! function_exists( 'chld_thm_cfg_parent_css' ) ):
+	function chld_thm_cfg_parent_css() {
+		wp_enqueue_style( 'chld_thm_cfg_parent', trailingslashit( get_template_directory_uri() ) . 'style.css', [
+			'bootstrap',
+			'bootstrap-theme',
+			'slicknav',
+			'foundation-icons',
+		] );
+		wp_enqueue_style( 'style_css', get_theme_root_uri() . 'style.css', [
+			'bootstrap',
+			'bootstrap-theme',
+			'slicknav',
+			'foundation-icons',
+		] );
+	}
 endif;
+
 add_action( 'wp_enqueue_scripts', 'chld_thm_cfg_parent_css', 10 );
+
+add_filter( 'gform_enable_field_label_visibility_settings', '__return_true' );
+
 
 // END ENQUEUE PARENT ACTION
 //
 // Your code goes below
 //
-if( function_exists('acf_add_options_page') ) {
-	acf_add_options_page();
+if ( function_exists( 'acf_add_options_page' ) ) {
+
+	$args = [
+
+		/* (string) The title displayed on the options page. Required. */
+		'page_title' => 'Site Options',
+
+		/* (string) The title displayed in the wp-admin sidebar. Defaults to page_title */
+		'menu_title' => '',
+
+		/* (string) The slug name to refer to this menu by (should be unique for this menu).
+		Defaults to a url friendly version of menu_slug */
+		'menu_slug'  => '',
+
+		/* (string) The capability required for this menu to be displayed to the user. Defaults to edit_posts.
+		Read more about capability here: http://codex.wordpress.org/Roles_and_Capabilities */
+		'capability' => 'manage_options',
+
+		'position'    => false,
+
+		/* (string) The slug of another WP admin page. if set, this will become a child page. */
+		'parent_slug' => '',
+
+		'icon_url' => 'dashicons-admin-site',
+
+		'post_id'  => 'site-options',
+		'autoload' => false,
+
+	];
+	acf_add_options_page( $args );
 }
--- home-page.php
+++ home-page.php
@@ -1,56 +1,56 @@
-<?php 
-/*********************************************** 
-* The main template file for the carzine theme
-***********************************************/
+<?php
+/***********************************************
+ * The main template file for the carzine theme
+ ***********************************************/
 
 /*
 
  * Template Name: Homepage
 
  */
-get_header();?>
-<div id="carzine_main_content_area">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <div id="carzine_main_content_container">
+get_header(); ?>
+    <div id="carzine_main_content_area">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div id="carzine_main_content_container">
 
-                    <?php if (get_field('slider_shortcode')) {
-                        $data = get_field('slider_shortcode');
-                        echo do_shortcode($data);
-                    } ?>
-                    <?php //get_template_part('theme_includes/featured_post_area');?>
-                    <?php //get_template_part('theme_includes/latest_post_area');?>
+						<?php if ( get_field( 'slider_shortcode' ) ) {
+							$data = get_field( 'slider_shortcode' );
+							echo do_shortcode( $data );
+						} ?>
+						<?php //get_template_part('theme_includes/featured_post_area');?>
+						<?php //get_template_part('theme_includes/latest_post_area');?>
+                    </div>
                 </div>
-            </div> 
 
                 <div class="col-md-12">
 
                     <div id="carzine_main_content_container">
 
-                        <?php get_template_part('theme_includes/single_page_area');?>
+						<?php get_template_part( 'theme_includes/single_page_area' ); ?>
 
                     </div>
 
                 </div>
-<div class="col-md-8">
-                <div id="carzine_main_content_container">
-                    <?php get_template_part('theme_includes/featured_post_area');?>
-                    <?php get_template_part('theme_includes/latest_post_area');?>
+                <div class="col-md-8">
+                    <div id="carzine_main_content_container">
+						<?php //get_template_part('theme_includes/featured_post_area');?>
+						<?php get_template_part( 'theme_includes/latest_post_area' ); ?>
+                    </div>
                 </div>
-            </div> 
+                <div class="col-md-4">
+                    <div id="carzine_sidebar_container">
+						<?php get_sidebar(); ?>
+                    </div>
+                </div>
+            </div>
             <div class="col-md-4">
                 <div id="carzine_sidebar_container">
-                    <?php get_sidebar();?>
+					<?php //get_sidebar();?>
                 </div>
-            </div>   
-        </div>
-            <div class="col-md-4">
-                <div id="carzine_sidebar_container">
-                    <?php //get_sidebar();?>
-                </div>
-            </div>   
+            </div>
         </div>
     </div>
-</div>
-<?php get_footer();?>
+    </div>
+<?php get_footer(); ?>
