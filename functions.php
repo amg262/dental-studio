b217583e6ad95ec7b7afa8d756ff35c9aa0b9841
Andrew Gunn <andrewmgunn26@gmail.com>
2017-05-24 01:07:27 -0500

    update

--- functions.php
+++ functions.php
@@ -41,12 +41,10 @@ if ( function_exists( 'acf_add_options_page' ) ) {
 		'page_title' => 'Site Options',
 
 		/* (string) The title displayed in the wp-admin sidebar. Defaults to page_title */
-		'menu_title' => '',
-
+		'menu_title' => 'Site Options',
 		/* (string) The slug name to refer to this menu by (should be unique for this menu).
 		Defaults to a url friendly version of menu_slug */
 		'menu_slug'  => '',
-
 		/* (string) The capability required for this menu to be displayed to the user. Defaults to edit_posts.
 		Read more about capability here: http://codex.wordpress.org/Roles_and_Capabilities */
 		'capability' => 'manage_options',
@@ -55,10 +53,9 @@ if ( function_exists( 'acf_add_options_page' ) ) {
 
 		/* (string) The slug of another WP admin page. if set, this will become a child page. */
 		'parent_slug' => '',
-
 		'icon_url' => 'dashicons-admin-site',
 
-		'post_id'  => 'site-options',
+
 		'autoload' => false,
 
 	];
--- home-page.php
+++ home-page.php
@@ -8,48 +8,92 @@
  * Template Name: Homepage
 
  */
-get_header();?>
-<div id="carzine_main_content_area">
+get_header(); ?>
+    <div id="carzine_main_content_area">
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
                     <div id="carzine_main_content_container">
 
-<?php if (get_field('slider_shortcode')) {
-	$data = get_field('slider_shortcode');
-	echo do_shortcode($data);
-}?>
+						<?php if ( get_field( 'slider_shortcode' ) ) {
+							$data = get_field( 'slider_shortcode' );
+							echo do_shortcode( $data );
+						} ?>
 						<?php //get_template_part('theme_includes/featured_post_area');?>
 						<?php //get_template_part('theme_includes/latest_post_area');?>
-</div>
+                    </div>
+                </div>
+
+                <div class="col-md-12">
+					<?php if ( have_rows( 'buckets' ) ):
+
+						$count = count( get_field( 'buckets' ) );
+
+						while ( have_rows( 'buckets' ) ) : the_row();
+
+							// Your loop code
+							$title   = get_sub_field( 'title' );
+							$image   = get_sub_field( 'image' );
+							$content = get_sub_field( 'content' );
+							$col     = 'col-md-4';
+
+							if ( $count = 1 ) {
+								$col = 'col-md-4';
+							} else if ( $count = 2 ) {
+								$col = 'col-md-6';
+							} else if ( $count = 3 ) {
+								$col = 'col-md-4';
+							} else if ( $count = 4 ) {
+								$col = 'col-md-3';
+							}
+
+							//$link    = get_sub_field( 'link' ); ?>
+
+
+                            <div class="<?php _e( $col, 'dental-studio' ); ?>">
+                                <h4>
+									<?php echo $count;//_e( $content, 'dental-studio' ); ?>
+
+                                </h4>
+                                <p>
+                                    <img src="<?php _e( $image['url'], 'dental-studio' ); ?>"
+                                         alt="<?php _e( $image['alt'], 'dental-studio' ); ?>"
+                                         width="128" height="128"/>
+
+									<?php _e( $content, 'dental-studio' ); ?>
+                                </p>
+                            </div>
+
+						<?php endwhile;
+
+					endif; ?>
                 </div>
 
                 <div class="col-md-12">
 
                     <div id="carzine_main_content_container">
 
-<?php get_template_part('theme_includes/single_page_area');?>
-</div>
+						<?php get_template_part( 'theme_includes/single_page_area' ); ?>
+                    </div>
 
                 </div>
                 <div class="col-md-9">
                     <div id="carzine_main_content_container">
-<?php //get_template_part('theme_includes/featured_post_area');?>
+						<?php //get_template_part('theme_includes/featured_post_area');?>
 						<?php //get_template_part( 'theme_includes/latest_post_area' ); ?>
-</div>
+                    </div>
                 </div>
                 <div class="col-md-3">
                     <div id="carzine_sidebar_container">
-<?php //get_sidebar(); ?>
-</div>
+						<?php //get_sidebar(); ?>
+                    </div>
                 </div>
             </div>
             <div class="col-md-4">
                 <div id="carzine_sidebar_container">
-<?php //get_sidebar();?>
-</div>
+					<?php //get_sidebar();?>
+                </div>
             </div>
         </div>
     </div>
-    </div>
-<?php get_footer();?>
+<?php get_footer(); ?>
